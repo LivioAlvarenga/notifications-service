378c4801162e904b488f19b2caf037a6c7eb581b
Livio Alvarenga <livioalvarenga@gmail.com>
2022-12-14 11:20:12 -0300

    emulando um banco de dados com desacoplamento de camadas

--- src/app/use-cases/send-notification.spec.ts
+++ src/app/use-cases/send-notification.spec.ts
@@ -1,15 +1,27 @@
+import { Notification } from '../entities/notification';
 import { SendNotification } from './send-notification';
+
+// Emulando um banco de dados (Desacoplamento de camadas)...
+const notifications: Notification[] = [];
+
+const notificationsRepository = {
+  async create(notification: Notification) {
+    notifications.push(notification);
+  },
+};
 
 describe('Send notification', () => {
   test('Ser capaz de enviar uma notificação', async () => {
-    const sendNotification = new SendNotification();
+    const sendNotification = new SendNotification(notificationsRepository);
 
-    const { notification } = await sendNotification.execute({
+    await sendNotification.execute({
       content: 'Notificação de teste',
       category: 'social teste',
       recipientId: 'recipient-id-teste',
     });
 
-    expect(notification).toBeTruthy();
+    console.log(notifications);
+
+    expect(notifications).toHaveLength(1);
   });
 });
