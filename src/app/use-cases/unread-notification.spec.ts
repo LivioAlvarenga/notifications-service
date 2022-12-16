7542c9491cec265c0f6896275186bfa7e98be893
Livio Alvarenga <livioalvarenga@gmail.com>
2022-12-16 10:39:14 -0300

    add marcar notificação como não lida

---
+++ src/app/use-cases/unread-notification.spec.ts
@@ -0,0 +1,34 @@
+import { makeNotification } from '@test/factories/notification-factory';
+import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
+import { NotificationNotFound } from './errors/notification-not-found';
+import { UnreadNotification } from './unread-notification';
+
+describe('Unread notification', () => {
+  test('Ser capaz de marcar uma notificação como não lida', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const unreadNotification = new UnreadNotification(notificationsRepository);
+
+    const notification = makeNotification({
+      readAt: new Date(),
+    });
+
+    await notificationsRepository.create(notification);
+
+    await unreadNotification.execute({
+      notificationId: notification.id,
+    });
+
+    expect(notificationsRepository.notifications[0].readAt).toBeNull();
+  });
+
+  test('Não ser capaz de marcar uma notificação como não lida que não existe', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const unreadNotification = new UnreadNotification(notificationsRepository);
+
+    expect(() => {
+      return unreadNotification.execute({
+        notificationId: 'fake-notification-id',
+      });
+    }).rejects.toThrow(NotificationNotFound);
+  });
+});
